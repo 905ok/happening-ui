653be3f052057b58477f2c7dfe60fe213c2c2503
jaikev <su.strata@gmail.com>
2022-01-01 18:17:08 +0800

    fix return system
    Happenings return each shapes data in their respective array. This
    seems to conflict with objects returning two kinds of data, e.g. a
    text box that returns a text and a rectangle shape. To solve this,
    Make a class for each shape type for determining the type. Then
    make a function that adds each data in its respective array by
    checking each its type.

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ const BG_COLOR = [164,231,19,1];
 const FG_COLOR = [54,61,36,1];
 const MAIN_WIN_BORDER_COLOR = [41,43,61,1];
 
-const rectangles = [];
+const returnData = {
+    particles: [],
+    lines: [],
+    rectangles: [],
+    polygons: [],
+    circles: [],
+    items: [],
+    texts: [],
+};
 
 function update(my) {
-    rectangles.push(...new Box(90, 70));
+    insertData(new Box(90, 70));
 
-    return {
-        rectangles
-    };
+    return returnData;
 }
 
 
@@ -23,14 +29,14 @@ function Box(width, height) {
     const borderThickness = 1;
     return [
         // rectangle
-        {
+        new rectangle({
             x1, y1, x2, y2,
             rgba: BG_COLOR,
             filled: true
-        },
+        }),
         
         // border
-        {
+        new rectangle({
             x1: (x1 - borderThickness),
             y1: (y1 - borderThickness),
             x2: (x2 + borderThickness),
@@ -39,6 +45,26 @@ function Box(width, height) {
             rgba: MAIN_WIN_BORDER_COLOR,
             filled: false
 
-        },
+        }),
     ];
 }
+
+
+function insertData(data) {
+    data.forEach(v => returnData[v.constructor.name+'s'].push(v));
+}
+
+
+class Data {
+    constructor(data) {
+        for (let k in data) this[k] = data[k];
+    }
+}
+
+class particle extends Data {}
+class line extends Data {}
+class rectangle extends Data {}
+class polygon extends Data {}
+class circle extends Data {}
+class item extends Data {}
+class text extends Data {}
